0cac6403a62ada9b2f004af01eef755a1efc9477
Valdenizio Brasil <123043399+valdenizio-brasil@users.noreply.github.com>
2023-09-12 00:24:44 +0000

    Aula switcase js

---
+++ switcase.js
@@ -0,0 +1,21 @@
+let dia = "terça"; // Você pode mudar isso para outros dias
+
+switch (dia) {
+  case "segunda":
+    console.log("Dia de começar a semana com energia!");
+    break;
+  case "terça":
+    console.log("Hora de aprender coisas novas!");
+    break;
+  case "quarta":
+    console.log("Metade da semana, continue firme!");
+    break;
+  case "quinta":
+    console.log("Quase lá, um pouco mais de esforço!");
+    break;
+  case "sexta":
+    console.log("Dia de comemorar, o fim de semana está chegando!");
+    break;
+  default:
+    console.log("Final de semana! Descanse e divirta-se!");
+}
